1acbfbb8f0ca33cf1ff4a60f52106e78d5fc402e
Alexis Yarbrough <alexisy0203@knights.ucf.edu>
2022-04-24 17:49:53 -0400

    styling changes

--- delete.php
+++ delete.php
@@ -10,16 +10,19 @@
         <h2>Are we out of a book?</h2>
         <form action="delete.php" method="POST">
             <h3>Delete a Book:</h3>
-            <label>Book Id:</label>  
-            <input type="number" id="book_id" name="book_id"></input>
-            <label>Book Name:</label>  
-            <input type="text" id="book_name" name="book_name"></input>
-            <input type="submit"></input>
+                <dl>
+                    <dt><label>Book Id:</label></dt>
+                        <dd><input type="number" id="book_id" name="book_id"></input></dd>
+                    <dt><label>Book Name:</label></dt>
+                        <dd><input type="text" id="book_name" name="book_name"></input></dd>
+                </dl>
+                <dd><input type="submit"></input></dd>
         </form>
     <?php
         connect();
         booksTable();
         deleteBook();
         close();
+        echo("<br><a href='index.php'>Back to Home</a>");
     ?>
 </html>
--- index.php
+++ index.php
@@ -10,23 +10,25 @@
     <h2>Current Book Stock</h2>
     <form action="index.php" method="POST">
         <h3>Add a Book to Our Stock:</h3>
-        <label>Book ID:</label>
-        <input type="number" id="book_id" name="book_id"></input>
-        <label>Book Name:</label>  
-        <input type="text" id="book_name" name="book_name"></input>
-        <label>Author Last Name:</label>  
-        <input type="text" id="author_lastname" name="author_lastname"></input>
-        <label>Author First Name:</label>  
-        <input type="text" id="author_firstname" name="author_firstname"></input>
-        <label>Genre:</label>
-        <input type="text" id="genre" name="genre"></input>
-        <input type="submit"></input>
+        <dl>
+            <dt><label>Book ID:</label></dt>
+                <dd><input type="number" id="book_id" name="book_id"></input></dd>
+            <dt><label>Book Name:</label><dt> 
+                <dd><input type="text" id="book_name" name="book_name"></input></dd>
+            <dt><label>Author Last Name:</label><dt>  
+                <dd><input type="text" id="author_lastname" name="author_lastname"></input></dd>
+            <dt><label>Author First Name:</label><dt>  
+                <dd><input type="text" id="author_firstname" name="author_firstname"></input></dd>
+            <dt><label>Genre:</label><dt>
+                <dd><input type="text" id="genre" name="genre"></input></dd>
+        </dl>
+            <dd><input type="submit"></input></dd>
     </form>
     <?php
         connect();
         booksTable();
         addBook();
         close();
-        echo("<a href='delete.php'>Search for a Book</a><br>");
+        echo("<br><a href='delete.php'>Delete a Book</a>");
     ?>
 </html>
